d6ab90fc69c5d372c0801097e596ca02a7e08532
Marco Giulietti <ga6t00n@gmail.com>
2020-11-03 16:48:26 +0100

    Second Snack done

--- js/main.js
+++ js/main.js
@@ -16,29 +16,29 @@ $(document).ready( function() {
   * 
   */
 
-let bicicles = [
-    {nome: 'bolt', peso: 25, colore: 'rosso'},
-    {nome: 'mountainview', peso: 45, colore: 'nero'},
-    {nome: 'feather', peso: 15, colore: 'giallo'}
-];
+// let bicicles = [
+//     {nome: 'bolt', peso: 25, colore: 'rosso'},
+//     {nome: 'mountainview', peso: 45, colore: 'nero'},
+//     {nome: 'feather', peso: 15, colore: 'giallo'}
+// ];
 
-// var peso = 0;
-// let biciLeggera = [];
-let ligthCycle = null;
-bicicles.forEach(element => {
-    if (ligthCycle == null || ligthCycle.peso > element.peso)  {
-        ligthCycle = element;
-    } 
-});
+// // var peso = 0;
+// // let biciLeggera = [];
+// let ligthCycle = null;
+// bicicles.forEach(element => {
+//     if (ligthCycle == null || ligthCycle.peso > element.peso)  {
+//         ligthCycle = element;
+//     } 
+// });
 
-const {nome,peso} = ligthCycle;
-    $('.content').html(
-        `<div>
-            <h3> Nome della bici più leggera: ${nome}</h3>
-            <h4> Peso della bici più leggera: ${peso}</h4>
-        </div>
-        `
-    );
+// const {nome,peso} = ligthCycle;
+    // $('.content').html(
+    //     `<div>
+    //         <h3> Nome della bici più leggera: ${nome}</h3>
+    //         <h4> Peso della bici più leggera: ${peso}</h4>
+    //     </div>
+    //     `
+    // );
 
 
  /**
@@ -54,35 +54,23 @@ const {nome,peso} = ligthCycle;
 
 
 
-//   var names = ['Franco', 'Luigi', 'Filippo', 'Alessandro', 'Adele', 'Alessia'];
-//   var arrayVal = names.length - 1;
-//   var min = parseInt( prompt('Inserisci un valore da 1 a ' + arrayVal) );
-//   while (isNaN(min) || min < 1 || min > arrayVal) {
-//     min = parseInt( prompt('Inserisci un valore da 1 a ' + arrayVal) );
-//   }
-//   var max = parseInt( prompt('Inserisci un valore da 1 a ' + arrayVal) );
-//   while (isNaN(max) || max < min || max > arrayVal) {
-//     max = parseInt( prompt('Inserisci un valore da 1 a ' + arrayVal) );
-//   }
-//   var newNames = [];
-//   newNames = names.forEach( (element, index) => {
-//       if (index >= min && index <= max) {
-//           console.log(element);
-//         newNames = element[index];
-//       } 
-      
-//       return  newNames
-//   });
-
-  
-  // var newNames = [];
-  // for ( var i = 0; i < names.length; i++) {
-  //   if( min <= i && max >= i) {
-  //     newNames.push(names[i]);
-  //   }
-  // }
-
-//  alert('I nomi selezionati nel range tra ' + min + ' e ' + max + ' sono:\n ' + newNames);
+  const names = ['Franco', 'Luigi', 'Filippo', 'Alessandro', 'Adele', 'Alessia'];
+  const arrayVal = names.length - 1;
+  const min = parseInt( prompt('Inserisci un valore da 1 a ' + arrayVal) );
+  while (isNaN(min) || min < 1 || min > arrayVal) {
+    min = parseInt( prompt('Inserisci un valore da 1 a ' + arrayVal) );
+  }
+  const max = parseInt( prompt('Inserisci un valore da 1 a ' + arrayVal) );
+  while (isNaN(max) || max < min || max > arrayVal) {
+    max = parseInt( prompt('Inserisci un valore da 1 a ' + arrayVal) );
+  }
+  let newNames = [];
+  newNames = names.filter( (element, index) => {
+      if (index >= min && index <= max) {
+          return element
+    } 
+  });
+alert('I nomi selezionati nel range tra ' + min + ' e ' + max + ' sono:\n' + newNames);
 
 
  /**
