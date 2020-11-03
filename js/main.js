4bb76c60639ba6a65eafaeedd117de43c76a4c49
Marco Giulietti <ga6t00n@gmail.com>
2020-11-03 14:09:42 +0100

    Start

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * 
+ *                      SNACKS WITH ES6 03/11/20
+ * 
+ *******************************************************************************/
+
+$(document).ready( function() {
+
+    /**
+     * 
+     * JSnack 1
+     * Creare un array di oggetti
+     * Ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+     * nome e peso.
+     * Stampare a schermo la bici con peso minore con handlebars.
+     * 
+     */
+   
+  
+   
+    /**
+     * 
+     * JSnack 2
+     * Partendo da un array di nomi (scelti da voi) creiamo un nuovo array.
+     * Chiedere due numeri all'utente per definire la posizione iniziale e
+     * finale dal quale estrarre i valori dall'array originale.
+     * Il nuovo array deve contenere gli elementi dell'array originale aventi
+     * la posizione (indice array) compresa tra i due numeri inseriti dall'utente.
+     * 
+     */
+   
+  
+   
+    /**
+     * 
+     * JSnack 3
+     * Creiamo un array di oggetti (scelti da voi)
+     * Creiamo una copia da questo array di partenza e aggiungiamo ai singoli
+     * elementi una nuova proprietà position che contiene una lettera casuale.
+     * Non dobbiamo modificare l'array iniziale.
+     * 
+     */
+   
+ 
+   });
